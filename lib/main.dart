45a50627baeb65a4657e9aefdbe47906361609a0
Akshay <akshayyadav7613@gmail.com>
2019-03-26 23:10:33 +0530

    stop..

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String>products=["food"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children:[Card(
+        body: Column(children: [
+      Card(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 100,),
+            SizedBox(
+              height: 100,
+            ),
             Container(
               width: 340,
               height: 40,
-              color: Colors.purple,),
+              color: Colors.purple,
+            ),
             Text("this card"),
             Container(
               width: 340,
               height: 40,
-              color: Colors.redAccent,),
+              color: Colors.redAccent,
+            ),
           ],
         ),
-      ),])
-    );
+      ),
+    ]));
   }
 }
