50a98972d2054d2223e4ba32f7e515cd78132036
Akshay <akshayyadav7613@gmail.com>
2019-03-27 22:32:27 +0530

    fix main.dart..

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,9 @@
 import 'package:flutter/material.dart';
 
+import './products_manager.dart';
+
 main() {
   runApp(MaterialApp(
-    home: MyApp(),
+    home: productsManager(),
   ));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ["food"];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(children: [
-      SizedBox(
-        height: 60,
-      ),
-      RaisedButton(
-        onPressed: () {
-          setState(() {
-            _products.add("this colors");
-          });
-        },
-        child: Text("add"),
-      ),
-    Column(children:_products.map((Element)=>  Card(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: 340,
-            height: 40,
-            color: Colors.purple,
-          ),
-          Text("this card"),
-        ],
-      ),
-    )).toList(),
-
-    ),
-
-    ]));
-  }
 }
