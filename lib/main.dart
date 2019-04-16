c7c3548d298abd36f9abec3ff9451bd9a677b954
Akshay <akshayyadav7613@gmail.com>
2019-04-16 20:52:41 +0530

    added debugpaintsizeenable..

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import './products_manager.dart';
 
 main() {
    debugPaintSizeEnabled = true;
+   debugPaintPointersEnabled = true;
   runApp(MaterialApp(
+    debugShowMaterialGrid: true,
    theme: ThemeData(
      primarySwatch: Colors.red
    ),
